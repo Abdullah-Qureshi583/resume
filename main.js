a196119159b113c476cf83e60d00b21642d661c3
Abdullah Mubeen <mabdullahqureshi583@gmail.com>
2024-09-08 06:46:29 +0500

    updated

--- main.js
+++
@@ -1,7 +0,0 @@
-var hamburger = document.querySelector(".hamburger");
-hamburger === null || hamburger === void 0 ? void 0 : hamburger.addEventListener("click", function () {
-    var r = document.querySelectorAll(".navUl");
-    r.classList.add("displayUl");
-    console.log(r);
-    console.log("clicked!");
-});
--- main.ts
+++
@@ -1,8 +0,0 @@
-let hamburger = document.querySelector(".hamburger");
-
-hamburger?.addEventListener("click",()=>{
-    let r:any = document.querySelectorAll(".navUl")
-    r.classList.add("displayUl")
-    console.log(r)
-    console.log("clicked!")
-})
